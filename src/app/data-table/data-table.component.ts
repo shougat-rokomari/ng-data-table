02dc84b1402706565e84980de12fcc06b66889b3
Shougat Hossain <shougat@rokomari.com>
2017-11-08 08:25:55 +0600

    Add pagination with active page (little bug)

--- src/app/data-table/data-table.component.ts
+++ src/app/data-table/data-table.component.ts
@@ -10,24 +10,42 @@ import {User} from '../domain/user';
 export class DataTableComponent implements OnInit {
   @Input() url: string;
   @Input() pageSize: number;
+  pages: number[] = [];
+  lastPage: number;
   currentPage = 1;
   users: User[];
   loading = true;
+
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
     setTimeout(() => {
-      this.http.get<User[]>(this.url + '?page=' + this.currentPage + '&size=' + this.pageSize).subscribe(data => {
-        this.users = data;
-        this.currentPage++;
-        this.loading = false;
-      });
+      this.http
+        .get<User[]>(this.url + '?page=' + this.currentPage + '&size=' + this.pageSize, {observe: 'response'})
+        .subscribe(response => {
+          this.users = response.body;
+          this.lastPage = Math.ceil(parseInt(response.headers.get('total-items'), 10) / this.pageSize);
+          for (let i = 1; i <= this.lastPage; i++) {
+            this.pages.push(i);
+          }
+          this.currentPage++;
+          this.loading = false;
+        });
     }, 1000);
   }
 
   loadNext() {
-    this.currentPage++;
-    this.loadPage(this.currentPage);
+    if (this.currentPage < this.lastPage) {
+      this.currentPage++;
+      this.loadPage(this.currentPage);
+    }
+  }
+
+  loadPrevious() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.loadPage(this.currentPage);
+    }
   }
 
   loadPage(page: number) {
